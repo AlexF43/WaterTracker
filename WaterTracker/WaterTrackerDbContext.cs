7813a85053a8fab1863f52ae87c3e912bbae7e7d
Dablestar <jbynchoi@gmail.com>
2024-10-18 00:01:30 +1100

    added model classes

--- WaterTracker/WaterTrackerDbContext.cs
+++ WaterTracker/WaterTrackerDbContext.cs
@@ -1,3 +1,5 @@
+using System.Net;
+using System.Security;
 using Microsoft.EntityFrameworkCore;
 
 namespace WaterTracker;
@@ -8,6 +10,52 @@ public class WaterTrackerDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder options)
         => options.UseSqlite("Data Source=watertrackerdatabase.db");
+}
+
+class User
+{
+    private string userId;
+
+    private SecureString userPwd;
+    User(string userId, string pwd)
+    {
+        this.userId = userId;
+        userPwd = new NetworkCredential("", pwd).SecurePassword;
+    }
+
+}
+
+class WaterUsage
+{
+    public WaterUsage(string usageId, string userId, string usageName, DateTime date, string usageType, int usedSec, double totalUsage)
+    {
+        this.usageId = usageId;
+        this.userId = userId;
+        this.usageName = usageName;
+        this.date = date;
+        this.usageType = usageType;
+        this.usedSec = usedSec;
+        this.totalUsage = totalUsage;
+    }
+
+    private string usageId;
+    private string userId;
+    private string usageName;
+    private DateTime date;
+    private string usageType;
+    private int usedSec;
+    private double totalUsage;
+}
+
+class WaterAmount
+{
+    private string usageType;
+    private double usageLiterPerSec;
+    public WaterAmount(string usageType, double usageLiterPerSec)
+    {
+        this.usageType = usageType;
+        this.usageLiterPerSec = usageLiterPerSec;
+    }
 }
 
 // todo create model class, replace "modelclass" with class name and run following dotnet commands
