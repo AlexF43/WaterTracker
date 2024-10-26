747a1c43c2791e0eab492086f4a748734fe3e4aa
Dablestar <jbynchoi@gmail.com>
2024-10-26 18:18:04 +1100

    added water tracking controller file

---
+++ WaterTracker/Controller/WaterTrackingController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WaterTracker.Services;
+
+namespace WaterTracker.Controller;
+
+[ApiController]
+[Route("api/[controller]")]
+
+public class WaterTrackingController : ControllerBase
+{
+    private readonly WaterTrackerDbContext _context;
+    private readonly JWTService _jwtService;
+    
+    public WaterTrackingController(WaterTrackerDbContext context, JWTService jwtService)
+    {
+        _context = context;
+        _jwtService = jwtService;
+    }
+    
+    
+}
